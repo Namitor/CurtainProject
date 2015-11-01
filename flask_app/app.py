c04e17d5d5ea262b5e318efa32d55821bfb78566
jayvee-Air <jayveehe@gmail.com>
2015-11-02 01:05:53 +0800

    test in local

---
+++ flask_app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+__author__ = 'jayvee'
+
+app = Flask(__name__)
+
+
+@app.route('/demo', methods=['GET'])
+def test_demo():
+    return render_template('test_index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=2222,debug=True)
