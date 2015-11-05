07874acf207062a8a27c2ecd474aebf20db563e4
wangzi6147 <363177773@qq.com>
2015-11-05 22:34:58 +0800

    test extension with leancloud

--- TestExtension/scripts/test.js
+++ TestExtension/scripts/test.js
@@ -1,4 +1,18 @@
-﻿var mask = document.createElement("div");//js新建元素
+﻿
+
+AV.initialize('hG9GaSHPU0TRVt9PLfddF4vG', 'PYS0BJpBOafFEtDPjdtsoXF1');
+
+var TestObject = AV.Object.extend('TestObject');
+var testObject = new TestObject();
+testObject.save({
+  foo: 'bar'
+}, {
+  success: function(object) {
+    alert('LeanCloud works!');
+  }
+});
+
+var mask = document.createElement("div");//js新建元素
 mask.setAttribute("id", "maskDiv");//给元素加id
 mask.setAttribute("style", "position:fixed;right:50px;bottom:50px;");//悬浮
 //mask.onclick = hideMask;//给元素添加点击事件
